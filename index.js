f80c7c849d54993d40d0b31c843acbb09fdb4b89
prathamgalbale <150934471+prathamgalbale@users.noreply.github.com>
2024-01-11 23:42:43 +0530

    done with parallax and carousel

--- index.js
+++ index.js
@@ -45,6 +45,111 @@ document.getElementById('emailInput').addEventListener('submit', function(event)
 });
 
 
+const mainBottomSpecial = document.querySelector(".mainBottomSpecial");
+const bottomLineVideo = document.querySelector(".bottomLineVideo");
+
+document.addEventListener('mousemove', function (e) {
+    bottomLineVideo.style.left =  e.clientX  - 500 + 'px';
+    bottomLineVideo.style.top = e.clientY  - 500 + 'px';
+});
+
+mainBottomSpecial.addEventListener('mouseover', function () {
+    bottomLineVideo.style.opacity = '1';
+});
+
+mainBottomSpecial.addEventListener('mouseout', function () {
+    bottomLineVideo.style.opacity = '0';
+});
+
+
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const carouselItems = document.querySelectorAll('.carouselItemOne, .carouselItemTwo');
+
+prevButton.addEventListener('click', () => {
+    carouselItems.forEach(item => item.classList.toggle('hidden'));    
+});
+
+nextButton.addEventListener('click', () => {
+    carouselItems.forEach(item => item.classList.toggle('hidden'));
+});
+
+
+
+
+
+const responsiveCarousel = document.querySelector('.responsiveCarousel');
+let currentIndex = 0;
+
+document.querySelector('.responsiveMainBody').addEventListener('click', function () {
+    currentIndex = (currentIndex + 1) % 2; 
+    updateCarousel();
+});
+
+function updateCarousel() {
+    const newPosition = -currentIndex * 50 + '%';
+    responsiveCarousel.style.transform = 'translateX(' + newPosition + ')';
+}
+
+
+
+
+const parallaxElements = document.querySelectorAll('.parallaxEffect');
+  
+function handleParallax() {
+    parallaxElements.forEach((element) => {
+    const scrollPosition = window.scrollY;
+    const elementOffset = element.offsetTop;
+    let multiplier = 0.3; 
+  
+    if (element.classList.contains('stationary')) {
+        multiplier = 0.13; 
+    }
+  
+    const distance = (scrollPosition - elementOffset) * multiplier;
+  
+    element.style.transform = `translateY(${distance}px)`;
+    });
+}
+  
+window.addEventListener('scroll', handleParallax);
+  
+handleParallax();
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const h2TextElements = document.querySelectorAll('.h2Text');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('translate');
+            } else {
+                entry.target.classList.remove('translate');
+            }
+        });
+    }, { threshold: 0.5 });
+
+    h2TextElements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+
+
+
+
+
+
+    
+      
+
+
+
+  
+  
 
 
 
